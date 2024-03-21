9c2490d777fd9187601046dfdf5e41d493aab37a
pluto <pluto@ao.xyz>
2024-03-21 10:53:19 +0900

    fix build error

--- src/EthStakeRegistry.sol
+++ src/EthStakeRegistry.sol
@@ -13,7 +13,7 @@ import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC16
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { Error } from "./utils/Error.sol";
 
-contract EthStakeRegistry is IEthStakeRegistry, Ownable, Error {
+contract EthStakeRegistry is IEthStakeRegistry, Ownable, ReentrancyGuard, Error {
     using ExcessivelySafeCall for address;
 
     IBlast public immutable BLAST;
